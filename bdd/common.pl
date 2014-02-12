54dec5120fb2b4f5e48ab3f8378879e0bbc0ff69
lucas8 <luc.linux@mailoo.org>
2014-02-12 21:49:34 +0100

    Improved top bar.

--- bdd/common.pl
+++ bdd/common.pl
@@ -1,10 +1,10 @@
 
 sub topbar {
     print '<div id="topbar">
-    <a href="menu.html#INDEX"class="toplink"><span>Index</span></a>
-    <a href="menu.html#BlOG" class="toplink_active"><span class="toplink_activespan">Blog</span></a>
-    <a href="menu.html#CODES" class="toplink"><span>Codes</span></a>
-    <a href="menu.html#PICTURES"class="toplink"><span>Pictures</span></a>
+    <a href="/index.pl"class="toplink"><span>Index</span></a>
+    <a href="/blog/menu.pl" class="toplink_active"><span class="toplink_activespan">Blog</span></a>
+    <a href="/codes/index.pl" class="toplink"><span>Codes</span></a>
+    <a href="/gallery/index.pl"class="toplink"><span>Gallery</span></a>
     </div>
     ';
 }
