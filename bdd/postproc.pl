70fef981710874bf2c669a79f3bcd3ffde708126
lucas8 <luc.linux@mailoo.org>
2014-03-21 09:38:48 +0100

    Added a script for preprocessing t2t articles.

---
+++ bdd/postproc.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+while(<>) {
+    my $line = $_;
+    $line =~ s/FIGURE\((.*),(.*)\)/```\n<div class="figure">\n\t<img class="picture" src="\2">\n\t<p class="label">\1<\/p>\n<\/div>\n```/;
+    print $line;
+}
+
