1717e331e5e0a11c6bfba3591700d8a2fbb0807c
lucas8 <luc.linux@mailoo.org>
2014-03-21 10:11:28 +0100

    Improved prepoc.pl.

--- bdd/preproc.pl
+++ bdd/preproc.pl
@@ -5,7 +5,7 @@ use warnings;
 
 while(<>) {
     my $line = $_;
-    $line =~ s/FIGURE\((.*),(.*)\)/```\n<div class="figure">\n\t<img class="picture" src="\2">\n\t<p class="label">\1<\/p>\n<\/div>\n```/;
+    $line =~ s/FIGURE\((.*),(.*)\)/```\n<div class="figure">\n\t<img class="picture" src="$2">\n\t<p class="label">$1<\/p>\n<\/div>\n```/;
     print $line;
 }
 
