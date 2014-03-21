a267c9c44b10286fb414336341d27c91a8ca952c
lucas8 <luc.linux@mailoo.org>
2014-03-21 23:19:27 +0100

    Improved list_articles to use xapian.

--- bdd/list-articles.pl
+++ bdd/list-articles.pl
@@ -2,30 +2,46 @@
 
 use warnings;
 use strict;
-use Encode;
-use utf8;
-use DBI;
+use Search::Xapian;
+my $dbdir = "/home/luc/Log/programmation/server/blog/xapian/";
 
-my $db     = 'blog';
-my $server = 'localhost';
-my $id     = 'luc';
-my $pwd    = 'PwD';
-my $port   = '';
-
-# Connecting
-print "Connecting to database $db.\n";
-my $dbh = DBI->connect("DBI:mysql:database=$db;host=$server;port=$port",
-    $id, $pwd, { RaiseError => 1, }
-) or die "Couldn't connect to database $db : $DBI::errstr";
-
-# Get all articles
-my $prep = $dbh->prepare('select * from articles;') or die $dbh->errstr();
-$prep->execute() or die 'Request failed !';
-while(my @row = $prep->fetchrow_array) {
-    print "@row\n";
+# Opening the database
+my $db = Search::Xapian::Database->new($dbdir);
+my $sort_by;
+if(scalar @ARGV and $ARGV[0] eq "-sdate") {
+    $sort_by = 0;
+    shift @ARGV;
 }
-$prep->finish();
 
-# Disconnecting
-$dbh->disconnect();
+# Opening and enquiring session
+my $enquire = Search::Xapian::Enquire->new($db);
+my $query_string = join ' ', @ARGV;
+my $qp = Search::Xapian::QueryParser->new();
+$qp->set_database($db);
+$qp->set_stemmer(Search::Xapian::Stem->new("french"));
+$qp->set_stemming_strategy(Search::Xapian::STEM_SOME);
+$qp->add_prefix("tag", 'T');
+my $vrpdate = new Search::Xapian::DateValueRangeProcessor(0, 1, 1920);
+$qp->add_valuerangeprocessor($vrpdate);
+
+# Parse the query.
+my $query = $qp->parse_query($query_string,
+    Search::Xapian::FLAG_PHRASE | Search::Xapian::FLAG_BOOLEAN | Search::Xapian::FLAG_LOVEHATE | Search::Xapian::FLAG_WILDCARD
+);
+$enquire->set_query($query);
+$enquire->set_sort_by_value($sort_by, 0) if defined $sort_by;
+
+# Return the top 10 results
+my $mset = $enquire->get_mset(0, 10);
+my $msize = $mset->size();
+die "No matching results found" if($msize == 0);
+
+# Showing the results
+printf "About %u matching documents were found.\n", $mset->get_matches_estimated();
+foreach my $m ($mset->items()) {
+    printf "#%u: Score %u%%: %s\n",
+    $m->get_rank() + 1,
+    $m->get_percent(),
+    $m->get_document()->get_data();
+}
 
