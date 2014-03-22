0d1ed4f5b5d99782012581c39cfbc0bfab724e05
lucas8 <luc.linux@mailoo.org>
2014-03-22 18:59:29 +0100

    Added support for videos in preproc.pl

--- bdd/preproc.pl
+++ bdd/preproc.pl
@@ -5,7 +5,8 @@ use warnings;
 
 while(<>) {
     my $line = $_;
-    $line =~ s/FIGURE\((.*),(.*)\)/'''\n<div class="figure">\n\t<img class="picture" src="$2">\n\t<p class="label">$1<\/p>\n<\/div>\n'''/;
+    $line =~ s/FIGURE\((.*),(.*)\)/'''\n<div class="figure">\n\t<img class="picture" src="$2" \/>\n\t<p class="label">$1<\/p>\n<\/div>\n'''/;
+    $line =~ s/VIDEO\((.*),(.*)\)/'''\n<div class="figure">\n\t<video class="movie" src="$2" controls preload=metadata \/>\n\t<p class="label">$1<\/p>\n<\/div>\n'''/;
     print $line;
 }
 
