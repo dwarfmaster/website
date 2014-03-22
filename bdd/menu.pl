ce1bd4c0397bc97e7e0ecbe2f1489fa4fb2b2df3
lucas8 <luc.linux@mailoo.org>
2014-03-22 10:47:06 +0100

    Updated menu.pl to use xapian.

--- bdd/menu.pl
+++ bdd/menu.pl
@@ -2,75 +2,60 @@
 
 use warnings;
 use strict;
-use Encode;
-use utf8;
-use DBI;
+use Search::Xapian;
 do '../utils/common.pl';
-
-my $db     = 'blog';
-my $server = 'localhost';
-my $id     = 'luc';
-my $pwd    = 'PwD';
-my $port   = '';
+my $dbdir = "/home/luc/Log/programmation/server/blog/xapian";
 
 # Connecting
-my $dbh = DBI->connect("DBI:mysql:database=$db;host=$server;port=$port",
-    $id, $pwd, { RaiseError => 1, }
-) or die "Couldn't connect to database $db : $DBI::errstr";
+my $db = Search::Xapian::Database->new($dbdir) or die "Couldn't open database.";
 
 # HTML
-head($dbh);
+head();
 
-my $sql_command = 'select id,path,date from articles';
-
-# Get max date
+# Get arguments
 my $query_string = $ENV{'QUERY_STRING'};
 $query_string = "" if not $query_string;
-my $date=undef;
-if($query_string =~ m/date=([0-9]{4}-[0-9]{2}-[0-9]{2}%20[0-9]{2}:[0-9]{2}:[0-9]{2})/) {
-    $date=$1;
-    $date =~ s/%20/ /g;
-    $sql_command .= " where date < \"$date\"";
+
+my $from = 0;
+if($query_string =~ m/from=([0-9]+)/) {
+    $from = $1;
 }
 
-my $prev_max=undef;
-$prev_max=" " if $query_string =~ m/prev=/;
-if($query_string =~ m/prev=([0-9]{4}-[0-9]{2}-[0-9]{2}%20[0-9]{2}:[0-9]{2}:[0-9]{2})/) {
-    my $prev_max=$1;
-    $prev_max =~ s/%20/ /g if $prev_max;
+my $query = "tag:all";
+if($query_string =~ m/query=([a-zA-Z(%20):]+)/) {
+    $query = $1;
 }
-
-# Get tag
-my $tag = undef;
-if($query_string =~ m/tag=([a-z]*)/) {
-    $sql_command .= " and" if $date;
-    $sql_command .= " where" if not $date;
-    $sql_command .= " tags regexp \"#$1\"";
-    $tag = $1;
-}
-
-# Get args
-my $args = undef;
-$args = $query_string if $query_string;
 
 # Get all articles
-$sql_command .= " order by date desc limit 6;";
-my $prep = $dbh->prepare($sql_command) or die $dbh->errstr();
-$prep->execute() or die 'Request failed !';
+my $enquire = Search::Xapian::Enquire->new($db);
+my $qp = Search::Xapian::QueryParser->new();
+$qp->set_database($db);
+$qp->set_stemmer(Search::Xapian::Stem->new("french"));
+$qp->set_stemming_strategy(Search::Xapian::STEM_SOME);
+$qp->add_prefix("tag", 'T');
+my $vrpdate = new Search::Xapian::DateValueRangeProcessor(0, 1, 1920);
+$qp->add_valuerangeprocessor($vrpdate);
 
-my $last_date="";
-my $nb=0;
-while(my @row = $prep->fetchrow_array) {
-    ++$nb;
-    article($row[1], $row[0], $args);
-    $last_date=$row[2];
+my $qr = $qp->parse_query($query,
+    Search::Xapian::FLAG_PHRASE | Search::Xapian::FLAG_BOOLEAN | Search::Xapian::FLAG_LOVEHATE | Search::Xapian::FLAG_WILDCARD
+);
+$enquire->set_query($qr);
+$enquire->set_sort_by_value(0, 0); # Sort by date
+
+my $mset = $enquire->get_mset($from, $from + 6);
+my $msize = $mset->size();
+my $args;
+if($query_string =~ m/^.*\?(.*)$/) {
+    $args = $1;
+} else {
+    $args = $query_string;
 }
-$prep->finish();
 
-footing($nb, $prev_max, $last_date, $date, $tag);
+foreach my $m ($mset->items()) {
+    article($m->get_document()->get_data(), $m->get_docid(), $args);
+}
 
-# Disconnecting
-$dbh->disconnect();
+footing($msize, $query, $from);
 
 sub head {
     print "Content-type: text/html\n\n";
@@ -111,30 +96,24 @@ sub head {
 }
 
 sub footing {
-    my ($nb, $prev, $next, $date, $tag) = @_;
-    if($prev or $nb == 6) {
-        print '<div id="navigation">';
-        if($prev) {
-            if($prev == " ") {
-                print "  <a class=\"prevpage\" href=\"menu.cgi";
-                print "?tag=$tag" if $tag;
-                print "\"> < Prev page </a>";
-            } else {
-                print "  <a class=\"prevpage\" href=\"menu.cgi?date=$prev";
-                print "&tag=$tag" if $tag;
-                print "\"> < Prev page </a>" if $prev != " ";
-            }
-        }
+    my ($nb, $query, $from) = @_;
 
-        if($nb == 6) {
-            print "  <a class=\"nextpage\" href=\"menu.cgi?date=$next";
-            print "&prev=$date" if $date;
-            print "&prev=" if not $date;
-            print "&tag=$tag" if $tag;
-            print "\"> Next page > </a>";
-        }
-        print '</div>';
+    print '<div id="navigation">' if $from > 0 or $nb == 6;
+    if($from > 0) {
+        my $fr = $from - 6;
+        print '  <a class="prevpage" href="menu.cgi?';
+        print "query=$query&" if $query ne "tag:all";
+        print "from=$fr" if $fr > 0;
+        print '"> < Prev page </a>';
     }
+
+    if($nb == 6) {
+        my $fr = $from + 6;
+        print '  <a class="nextpage" href="menu.cgi?';
+        print "query=$query&" if $query ne "tag:all";
+        print "from=$fr\"> Next page > </a>";
+    }
+    print '</div>' if $from > 0 or $nb == 6;
 
     print '</div>
     <!-- Foot -->
