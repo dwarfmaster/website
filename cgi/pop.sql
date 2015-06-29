bd9fd9eda64f6718a2e9eb6bf57c504e48ef8629
lucas8 <luc.linux@mailoo.org>
2015-06-29 17:36:30 +0200

    Update sql table model

--- cgi/pop.sql
+++ cgi/pop.sql
@@ -2,22 +2,12 @@ USE blog;
 
 CREATE TABLE IF NOT EXISTS Articles(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    path VARCHAR(25),
+    content TEXT,
     time DATETIME
     );
-INSERT INTO Articles(path, time) VALUES
-    ("introduction", "2014-09-19 12:00:00"),
-    ("lorem",        "2014-09-19 15:59:59"),
-    ("archlinux",    "2015-06-29 10:12:05");
 
 CREATE TABLE IF NOT EXISTS Tags(
     article INT,
-    tag VARCHAR(5)
+    tag VARCHAR(30)
     );
-INSERT INTO Tags(article, tag) VALUES
-    (1, "test"),
-    (1, "prog"),
-    (2, "test"),
-    (3, "linux"),
-    (3, "prog");
 
