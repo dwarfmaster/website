2626fbad9d41f7bd8abb10837f157cb709c3ebe6
lucas8 <luc.linux@mailoo.org>
2017-03-31 11:06:09 +0200

    Add gnu-mach-principles article

--- haskell/src/Articles.hs
+++ haskell/src/Articles.hs
@@ -46,6 +46,13 @@ prod =
         ["system", "hurd", "qemu", "prog"]
         (readDate 28 2 2017)
         "hacking-hurd-workflow"
+    , Article
+        "Understanding the GNU Mach API"
+        (toWidget $ $(whamletFile "articles/gnu-mach-principles/summary.hamlet"))
+        (toWidget $ $(whamletFile "articles/gnu-mach-principles/content.hamlet"))
+        ["system", "hurd", "mach", "prog"]
+        (readDate 31 3 2017)
+        "gnu-mach-principles"
     ]
 
 blog :: Blog
